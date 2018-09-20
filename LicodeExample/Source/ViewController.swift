aa4b3d5c7091c25144c3468d1f3ef7f3f455be5c
ztimc <ztimcz@gmail.com>
2018-09-20 10:53:18 +0800

    init

---
+++ LicodeExample/Source/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LicodeExample
+//
+//  Created by ztimc on 2018/9/20.
+//  Copyright © 2018年 ztimc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Source/licode.h
@@ -0,0 +1,19 @@
+//
+//  licode.h
+//  licode
+//
+//  Created by ztimc on 2018/9/19.
+//  Copyright © 2018年 ztimc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for licode.
+FOUNDATION_EXPORT double licodeVersionNumber;
+
+//! Project version string for licode.
+FOUNDATION_EXPORT const unsigned char licodeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <licode/PublicHeader.h>
+
+
